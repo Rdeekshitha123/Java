063d0fc54de53f5768b8fc72b4d643b28fdefb5a
Deekshitha <deekshithar1307@gmail.com>
2025-09-08 02:16:58 +0000

    Pipeline changes

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello");
+	System.out.println("Pipeline");
     }
 
     public int addNumbers(int a, int b) {
